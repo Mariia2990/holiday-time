7d0032776e73e3f9cc802ff2f36030875a5e766e
Mariia <masyakuk@gmail.com>
2026-02-26 11:06:23 +0200

    додано переклад до сторінки Контакти

--- src/pages/ContactsPage/ContactsPage.tsx
+++ src/pages/ContactsPage/ContactsPage.tsx
@@ -1,44 +1,43 @@
 import React from "react";
 import css from "./ContactsPage.module.css";
 import Container from "../../components/Container/Container";
+import { useTranslation } from "react-i18next";
 
 const ContactsPage: React.FC = () => {
+   const { t } = useTranslation();
   return (
     <>
-     
-        <div className={css.contactsBox}>
-          <h2 className={css.contactsTitle}>Контакти</h2>
+      <div className={css.contactsBox}>
+        <h2 className={css.contactsTitle}>{t("contactsTitle")}</h2>
       </div>
       <Container>
         <div className={css.container}>
           <div className={css.contactsContainer}>
             <div className={css.contactsItem}>
-              <h3 className={css.contactsLabel}>Телефон</h3>{" "}
+              <h3 className={css.contactsLabel}>{t("contactsLabel")}</h3>{" "}
               <span className={css.contactsSpan}></span>
               <p className={css.contactsInfo}>+38(097)119-89-83</p>
             </div>
             <div className={css.contactsItem}>
-              <h3 className={css.contactsLabel}>Графік роботи</h3>
+              <h3 className={css.contactsLabel}>{t("contactsLabe2")}</h3>
               <span className={css.contactsSpan1}></span>
               <p className={css.contactsInfo1}>
-                пн-пт 08:00-22:00 <br />
-                сб 09:00-21:00 <br />
-                нд 10:00-20:00
+                {t("contactsInfo2")} 08:00-22:00 <br />
+                {t("contactsInfo3")} 09:00-21:00 <br />
+                {t("contactsInfo4")} 10:00-20:00
               </p>
             </div>
 
             <div className={css.contactsItem}>
-              <h3 className={css.contactsLabel}>Ел. пошта</h3>
+              <h3 className={css.contactsLabel}>{t("contactsLabe3")}</h3>
               <span className={css.contactsSpan2}></span>
               <p className={css.contactsInfo2}>hello@gmail.com</p>
             </div>
 
             <div className={css.contactsItem}>
-              <h3 className={css.contactsLabel2}>Адреса офісу</h3>
+              <h3 className={css.contactsLabel2}>{t("contactsLabel4")}</h3>
               <span className={css.contactsSpan3}></span>
-              <p className={css.contactsInfo3}>
-                м. Київ, вул. Малевича, буд. 4
-              </p>
+              <p className={css.contactsInfo3}>{t("contactsInfo5")}</p>
             </div>
 
             <div className={css.contactsMap}>
